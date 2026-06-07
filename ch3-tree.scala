85d495f89329675b06d77519973c3b9e4453367c
harimadhavan2000 <34412472+harimadhavan2000@users.noreply.github.com>
2026-06-07 23:01:59 +0530

    add size to tree

--- ch3-tree.scala
+++ ch3-tree.scala
@@ -2,6 +2,15 @@ sealed trait Tree[+A]
 case class Leaf[A](value: A) extends Tree[A]
 case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
+object Tree {
+
+  def size[A](tree: Tree[A]): Int = tree match {
+    case Leaf(value) => 1
+    case Branch(left, right) => size(left) + size(right) + 1
+  }
+
+}
+
 object TreeApp {
 
   def main(args: Array[String]): Unit = {
@@ -12,6 +21,7 @@ object TreeApp {
       ),
       Leaf(3)
     )
+    println(Tree.size(sampleTree))
 
   }
 }
